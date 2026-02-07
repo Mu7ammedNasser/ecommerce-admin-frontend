3806a23449fa4db8fcaad5d6bb185fc684c095ae
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-07 15:10:56 +0200

    fix error

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,10 +6,10 @@ import { NotFounded } from './Components/not-founded/not-founded';
 import { Login } from './Components/login/login';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/products', pathMatch: 'full' },
-  { path: '/login', component: Login },
-  { path: '/products', component: Products },
-  { path: '/orders', component: Orders },
-  { path: '/customers', component: Users },
+  { path: '', redirectTo: 'products', pathMatch: 'full' },
+  { path: 'login', component: Login },
+  { path: 'products', component: Products },
+  { path: 'orders', component: Orders },
+  { path: 'customers', component: Users },
   { path: '**', component: NotFounded },
 ];
