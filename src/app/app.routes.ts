d61ac843177381f4a503e66369eb14a49a021b24
Muhammed Nasser <mo.nasser20032003@gmail.com>
2026-02-07 15:06:12 +0200

    routes and not founded component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { Products } from './Components/products/products';
+import { Orders } from './Components/orders/orders';
+import { Users } from './Components/users/users';
+import { NotFounded } from './Components/not-founded/not-founded';
+import { Login } from './Components/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/products', pathMatch: 'full' },
+  { path: '/login', component: Login },
+  { path: '/products', component: Products },
+  { path: '/orders', component: Orders },
+  { path: '/customers', component: Users },
+  { path: '**', component: NotFounded },
+];
